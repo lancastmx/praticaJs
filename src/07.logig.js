6fbebd1cc2a1df5fe0957728e7764e93ad1b28cf
lancast <angelprep92@gmail.com>
2026-03-12 00:51:54 -0600

    push basico

---
+++ src/07.logig.js
@@ -0,0 +1,18 @@
+// Operadores logicos
+
+// AND (&&)
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+// OR (||)
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+// NOT (!)
+console.log(!true); // false
+console.log(!false); // true
+
